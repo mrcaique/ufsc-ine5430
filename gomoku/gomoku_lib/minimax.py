babe08e52054825e36c0e3780a9873ede18811f6
Fernando Jorge Mota <f.j.mota13@gmail.com>
2016-09-20 00:17:13 -0300

    Remove unnecessary 'else:' block

--- gomoku/gomoku_lib/minimax.py
+++ gomoku/gomoku_lib/minimax.py
@@ -11,21 +11,20 @@ def minimax(state, depth, alpha, beta, is_player):
     if depth == 0 or next_state is finished:
         value = evaluate(state)
         return value, state
-    else:
-        while next_state is not finished:
-            if is_player:
-                value, _ = minimax(next_state, depth - 1, alpha, beta, False)
-                if value > alpha:
-                    alpha = value
-                    best_state = next_state
-            else:
-                value, _ = minimax(next_state, depth - 1, alpha, beta, True)
-                if value < beta:
-                    beta = value
-                    best_state = next_state
-            if alpha >= beta:
-                break
-            next_state = next(next_states, finished)
+    while next_state is not finished:
+        if is_player:
+            value, _ = minimax(next_state, depth - 1, alpha, beta, False)
+            if value > alpha:
+                alpha = value
+                best_state = next_state
+        else:
+            value, _ = minimax(next_state, depth - 1, alpha, beta, True)
+            if value < beta:
+                beta = value
+                best_state = next_state
+        if alpha >= beta:
+            break
+        next_state = next(next_states, finished)
     if is_player:
         return alpha, best_state
     else:
